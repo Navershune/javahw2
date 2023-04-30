5f456e698f319569af42a7bb564cc8719845ee1a
Ilia Firsov <i.fiirsov@gmail.com>
2023-04-30 11:34:14 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 12000;
+        int bonus = 20;
+        int mile = cost / bonus;
+        System.out.println("Количество бонусов = " + mile);
+    }
+}
